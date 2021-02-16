fa61e0615f265e818c47b4252d5de10a4bf1155e
ekg-21 <emma.gao2@outlook.com>
2021-02-17 10:08:20 +1100

    Test pull request via VScode

--- dice_roller.py
+++ dice_roller.py
@@ -1,10 +1,10 @@
 #Make sure to only edit within MAIN
-import random
+
 def main():
+  import random
   roll = random.randint(1,6)
   print(f'You rolled a {roll}')
-  #Output: You rolled a 3
-  print('Testing pull request')
+  
 
 if __name__== "__main__":
   main()
